d7f256a757cb739ab711a6dae4418719ae14b9aa
Cyril Aulagnon <syrryllo@gmail.com>
2021-10-23 10:59:22 +0200

    fixing-person-model

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,6 +1,8 @@
 class Person < ApplicationRecord
-  has_many  :person_technologies, 
-            :person_expertises,
-            :person_industries
+  has_many  :person_technologies 
+  has_many  :person_expertises
+  has_many  :person_industries
+
   belongs_to :origin
+  
 end
