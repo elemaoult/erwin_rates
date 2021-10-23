c82d461b62158ce9b04d9c82bffcb567cb7a0dc1
LY QUACH <lyllq.quach@gmail.com>
2021-10-23 18:04:20 +0200

    NES OK

--- app/controllers/persons_controller.rb
+++ app/controllers/persons_controller.rb
@@ -1,2 +1,18 @@
 class PersonsController < ApplicationController
+  
+  def new
+    @freelancer = Freelancer.new
+  end
+
+  def create
+    @freelancer = Freelancer.new(params[:freelancer])
+    @freelancer.save!
+    redirect_to freelancer_path(@freelancer)
+  end
+  
+  def destroy
+  end
+
+  def freelancer_params
+    params.require(:freelancer).permit(:first_name, :last_name, :location, :job_description, :experience, :daily_rate, :currency)
 end
