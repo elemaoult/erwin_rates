a353588cc6bccf34ed052df8a816f18f5bb5c14b
Emilie LeMaoult <emilie.lemaoult@gmail.com>
2021-11-08 09:55:03 +0100

    don't touch it'

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home, :freelancer_expertises_data]
-  skip_before_action :verify_authenticity_token, only: :freelancer_expertises_data
+  # skip_before_action :authenticate_user!, only: [:home, :freelancer_expertises_data]
+  # skip_before_action :verify_authenticity_token, only: :freelancer_expertises_data
   
   def home
     @expertises = []
