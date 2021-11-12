569c8553b2144a7ae9e1c71e2f2518619d3c53dd
Paul Portier <paulportier@gmail.com>
2021-11-12 12:25:08 +0100

    tidy up javascript

---
+++ app/javascript/components/pure_counter.js
@@ -0,0 +1,7 @@
+import PureCounter from '@srexi/purecounterjs/js/purecounter'
+
+const initPureCounter = () => {
+  const pure = new PureCounter
+}
+
+export { initPureCounter }
---
+++ app/javascript/components/validate.js
@@ -0,0 +1,5 @@
+const initValidate = () => {
+  var validate = require("validate.js");
+}
+
+export { initValidate }
