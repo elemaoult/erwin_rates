a6be2bb6931c4f223da1b7c19802b01b083d23c6
Emilie LeMaoult <emilie.lemaoult@gmail.com>
2021-11-13 11:38:43 +0100

    Installation done

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,7 +8,6 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
-
 require("particles.js")
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -16,6 +15,8 @@ require("particles.js")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+
 
 
 // ----------------------------------------------------
@@ -43,13 +44,13 @@ AOS.init();
 
 
 // Internal imports, e.g:
-// import { initSelect2 } from '../components/init_select2';
+import { initSelect2 } from '../components/init_select2';
 import { greatListener } from '../components/filter_listener';
 
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
-  // initSelect2();
+  initSelect2();
   greatListener();
 });
 
