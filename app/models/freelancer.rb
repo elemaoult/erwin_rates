60a356532ccf51386d2ea3ccb81b0ad3ce6edcd9
Cyril Aulagnon <syrryllo@gmail.com>
2021-11-16 20:13:40 +0100

    fixing daily rate interval generation

--- app/models/freelancer.rb
+++ app/models/freelancer.rb
@@ -2,15 +2,13 @@ class Freelancer < ApplicationRecord
 
   has_many  :freelancer_technologies, dependent: :destroy
   has_many  :freelancer_expertises, dependent: :destroy
-  has_many  :freelancer_industries, dependent: :destroy
   
   has_many :technologies, through: :freelancer_technologies, dependent: :destroy
   has_many :expertises, through: :freelancer_expertises, dependent: :destroy
-  has_many :industries, through: :freelancer_industries, dependent: :destroy
   
   belongs_to :source
   
-  before_update :round_50 
+  after_save :round_50 
   # scope :from_seed, -> { joins(:source).where('source.seed_valid = ?', false) }
 
   private
