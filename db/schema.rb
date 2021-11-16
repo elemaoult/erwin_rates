3fc07627e65a047ef3f110e5fb98f8734e626785
Cyril Aulagnon <syrryllo@gmail.com>
2021-11-16 20:35:09 +0100

    initial commit on branch

--- db/schema.rb
+++ db/schema.rb
@@ -94,15 +94,6 @@ ActiveRecord::Schema.define(version: 2021_11_13_170305) do
     t.index ["freelancer_id"], name: "index_freelancer_expertises_on_freelancer_id"
   end
 
-  create_table "freelancer_industries", force: :cascade do |t|
-    t.bigint "industry_id", null: false
-    t.bigint "freelancer_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["freelancer_id"], name: "index_freelancer_industries_on_freelancer_id"
-    t.index ["industry_id"], name: "index_freelancer_industries_on_industry_id"
-  end
-
   create_table "freelancer_technologies", force: :cascade do |t|
     t.bigint "technology_id", null: false
     t.bigint "freelancer_id", null: false
@@ -130,12 +121,6 @@ ActiveRecord::Schema.define(version: 2021_11_13_170305) do
     t.integer "daily_rate_interval"
     t.boolean "included_in_analysis", default: true
     t.index ["source_id"], name: "index_freelancers_on_source_id"
-  end
-
-  create_table "industries", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "reviews", force: :cascade do |t|
@@ -179,8 +164,6 @@ ActiveRecord::Schema.define(version: 2021_11_13_170305) do
 
   add_foreign_key "freelancer_expertises", "expertises"
   add_foreign_key "freelancer_expertises", "freelancers"
-  add_foreign_key "freelancer_industries", "freelancers"
-  add_foreign_key "freelancer_industries", "industries"
   add_foreign_key "freelancer_technologies", "freelancers"
   add_foreign_key "freelancer_technologies", "technologies"
   add_foreign_key "freelancers", "sources"
