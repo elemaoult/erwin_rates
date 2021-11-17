3031426469b35ae01a445fa6e5766fb81c5ae1f7
Cyril Aulagnon <syrryllo@gmail.com>
2021-11-17 20:22:54 +0100

    adding a remote button in selection

--- app/controllers/freelancers_controller.rb
+++ app/controllers/freelancers_controller.rb
@@ -32,65 +32,27 @@ class FreelancersController < ApplicationController
     @result = result.values.map{|res| {freelancer: res[1], count: res[0]}}
   end
 
-#   def filtered_freelancers
-# #  Get all occurences of params
-
-#     Freelancer.all.select do |freelance| 
-#       filter_condition?(freelance)
-#     end
-#   end
-
-#   def filter_condition?(freelance)
-#     condition_expertise?(freelance) && condition_technology?(freelance) && condition_experience?(freelance)
-#   end
-
-#   def condition_expertise?(freelance)
-#     return true if @filter_expertise == @all_expertises
-#     # ary = freelance.expertises.select do |expertise|
-#     #   @filter_expertise.include?(expertise.name)
-#     # end
-#     # !ary.empty?
-#     ary = freelance.expertises.map do |expertise|
-#       expertise.name
-#     end
-#     !(@filter_expertise - ary).empty? 
-#   end
-
-#   def condition_technology?(freelance)
-#     return true if @filter_technology == @all_technologies
-#     # ary = freelance.technologies.select do |technology|
-#     #   @filter_technology.include?(technology.name)
-#     # end
-#     # !ary.empty?
-#     ary = freelance.technologies.map do |technology|
-#       technology.name
-#     end
-#     !(@filter_technology - ary).empty? 
-#   end
-
-  # def condition_experience?(freelance)
-  #   return true if @filter_experience == @all_experiences
-  #   @filter_experience.include?(freelance.experience) 
-  # end
-
   def get_conditions
 
     #   Get all possible params
-    @all_expertises = Expertise.pluck(:name)
-    @all_technologies = Technology.pluck(:name)
-    @all_experiences = Freelancer.pluck(:experience)
-    @all_genders = Freelancer.pluck(:gender_guess)
+    @all_expertises = Expertise.pluck(:name).uniq
+    @all_technologies = Technology.pluck(:name).uniq
+    @all_experiences = Freelancer.pluck(:experience).uniq
+    @all_genders = Freelancer.pluck(:gender_guess).uniq
+    @all_remotes = Freelancer.pluck(:remote).uniq
 
     #   Get params from form
     @filter_expertise = params["Expertises"].blank? ? @all_expertises : [params["Expertises"]]
     @filter_technology = params["Technologies"].blank? ? @all_technologies : [params["Technologies"]]
     @filter_experience = params["Seniority"].blank? ? @all_experiences : [params["Seniority"]]
     @filter_gender = params["Gender"].blank? ? @all_genders : [params["Gender"]]
+    @filter_remote = params["Remote"].blank? ? @all_remotes : ( [params["Remote"]] == ["Présentiel"] ? false : true )
+
   end
 
   def freelancer_expertises_data  
     
-    big_joined_table = Freelancer.joins(:technologies, :expertises).where(experience: @filter_experience, gender_guess: @filter_gender, technologies:{name: @filter_technology }, expertises: {name: @filter_expertise }).distinct
+    big_joined_table = Freelancer.joins(:technologies, :expertises).where(experience: @filter_experience, gender_guess: @filter_gender, remote: @filter_remote, technologies:{name: @filter_technology }, expertises: {name: @filter_expertise }).distinct
     hsh = big_joined_table.group(:daily_rate_interval).count
     # [{:daily_rate_interval => "400", :count=>25},{:daily_rate_interval => "500", :count=>50}]
     # {0=>3, 150=>1, 200=>1, 300=>7, 350=>19, 400=>12, 450=>12, 500=>10, 550=>14, 600=>14, 650=>4, 700=>7, 750=>6, 800=>3, 850=>2, 900=>4}
