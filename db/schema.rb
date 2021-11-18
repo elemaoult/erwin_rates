2398449ef44adc6c0edfa02f19f7f66f7ae825c4
LY QUACH <lyllq.quach@gmail.com>
2021-11-18 21:37:35 +0100

    petite_coloration_search

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 2021_11_13_104920) do
-=======
 ActiveRecord::Schema.define(version: 2021_11_18_093206) do
->>>>>>> 0b5b61c47038eeadc7ed7f7e4cb13a064abb65a9
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
